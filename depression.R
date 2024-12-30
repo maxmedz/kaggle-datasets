5f4299fdc57523a080e64e2dd8ca8fc12095608e
maxmedz <mrmedzmariashvili@gmail.com>
2024-12-30 12:40:18 +0400

    upon running the logistic regression. no further expanations

--- depression.R
+++ depression.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(ggcorrplot)
+
 x<-read_csv("Data/Depression Professional Dataset.csv")
 glimpse(x)
 ##since depression is the categorical variable this project is to work with the logistic regression.
@@ -24,12 +26,27 @@ df<-mutate(df,
            suicth=ifelse(suicth=="Yes",1,0),
            gen=ifelse(gen=="Yes",1,0),
            depression=ifelse(depression=="Yes",1,0),
-           sleep=factor(sleep,
+           sleepf=factor(sleep,
                          levels = c("Less than 5 hours","5-6 hours","7-8 hours", "More than 8 hours"),
                          ordered = TRUE
                         ),
-           diets=factor(diets,
+           dietsf=factor(diets,
                        levels = c("Unhealthy","Moderate","Healthy"),
-                       ordered = TRUE)
+                       ordered = TRUE),
+           sleep=(as.numeric(sleepf)),
+           diets=(as.numeric(dietsf))
 )
+#now let's create some correlations
+correlation_matrix <- cor(select(df,where(is.numeric)), use = "complete.obs")  # Exclude rows with missing values
+# Print the raw correlation matrix
+correlation_matrix
+# Visualize the correlation matrix with labels
+ggcorrplot(correlation_matrix, lab = TRUE)
 
+#let's run the logistic regression ans see how it performs
+m1 <- glm(
+  depression ~ sex + age + wrkpr + jobsat + sleep + diets + suicth + wrkhrs + finstr + gen,
+  data = df,
+  family = binomial(link = "logit")
+)
+texreg::screenreg(m1)
